ed37a889560fe612338b2e961c5c740f45b28b3c
Priyantama <rizkipriyantama1213@gmail.com>
2020-12-28 08:41:18 +0700

    tambah siapakita

--- resources/views/about/siapakita.blade.php
+++ resources/views/about/siapakita.blade.php
@@ -11,16 +11,15 @@
 <main>
     <section class="siapakita">
         <div class="container">
-          <img src="img/AssetSiapaKita/Group 2.png" class="img-background-2">
             <div class="jumbotron">
-            
               <h1 class="display-4">APA ITU S.I.P.A</h1>
-
-            </div>
-                  <img class="img1" src="img/AssetSiapaKita/sipa3.png" width="300">
+             </div>
+                  
                       <div class="row">
-                          <div class="col-sm-5"></div>
-                          <div class="col-sm-7"><p>Sistem Informasi Perizinan Acara adalah sistem yang akan membantu kamu
+                          <div class="gambar">
+                          <img class="img1" src="img/AssetSiapaKita/sipa3.png" width="360" >
+                          </div>
+                          <div class="col-md-2col-sm-8 text1"><p>Sistem Informasi Perizinan Acara adalah sistem yang akan membantu kamu
                             dalam mengurus persuratan dan perizinan ketika kamu ingin membuat acara. Dari mulai acara formal,
                             non-formal, sampai acara keagamaan.
                           </p>
@@ -28,14 +27,12 @@
                               <i>"Proffesional"</i> menjadi komitmen bagi kami untuk membantu, melayani dan juga selalu 
                               memberikan yang terbaik dengan standar tertinggi layanan untuk pencapaian hasil terbaik
                           </p>
+                          <div class="text2">
+                          <p> <i>"Easy and Simple"</i></p>
+                        </div>
                           </div> 
                       </div>
 
-                          <div class="row">
-                                <div class="col-sm-7"></div>
-                                    <div class="col-sm-5">"Easy and Simple"
-                                    </div>
-                              </div>
                                 <div class="col-btn">
                                 <a href="{{url('/event')}}" class="btn btn-1" >Join</a>
                               </div>
